358f3acae23497ee8fd90572d6c093b77c04539f
Brandon Learned <blearned92@gmail.com>
2022-01-14 14:01:47 -0700

    Finished game, tweaked game, added README.md. Added starting menu to game.

---
+++ README.md
@@ -0,0 +1,32 @@
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Images](#images)
+
+## General info
+This project is a terminal game project where I decided to learn how to make the game of Hangman. Be assigned a random word and guess away with letters (and only letters!) to discover the correct word before leading a poor stickman to drawing his last breath! 
+
+Within this project, I reestablished my fundamentals of python by practices loops and instantiating functions correctly, while working on problem solving skills to make everything work as intended. Troubles I faced were ensuring that guesses could only be letters and making sure that a word with a hyphen would be represented properly within the terminal alongside the underscores. I also learned how to use images in markdown!
+	
+## Technologies
+Project is created with:
+* Python 3.9.7
+
+	
+## Setup
+Run in a prefered IDE such as [Visual Studio Code](https://code.visualstudio.com/)
+
+## Images
+View the rules before you start!
+
+![Hangman Rules](hangmanRules.JPG)
+
+Play a friendly game of Hangman in your terminal!
+
+![Play a friendly game of Hangman in your Terminal!](hangman1.JPG)
+
+Look! You've won!
+
+
+![Look! You've won!](hangmanWin.JPG)
--- terminalGame.py
+++ terminalGame.py
@@ -1,6 +1,7 @@
 import random
 
 def print_0_mistakes():
+    print("-------------------------------------------")
     print("  |------|-")
     print("  |      | ")
     print("  |        ")
@@ -11,6 +12,7 @@ def print_0_mistakes():
     print("/ | \\      ")
 
 def print_1_mistakes():
+    print("-------------------------------------------")
     print("  |------|-")
     print("  |      | ")
     print("  |      O ")
@@ -21,6 +23,7 @@ def print_1_mistakes():
     print("/ | \\      ")
 
 def print_2_mistakes():
+    print("-------------------------------------------")
     print("  |------|-")
     print("  |      | ")
     print("  |      O ")
@@ -31,6 +34,7 @@ def print_2_mistakes():
     print("/ | \\      ")
 
 def print_3_mistakes():
+    print("-------------------------------------------")
     print("  |------|-")
     print("  |      | ")
     print("  |      O ")
@@ -41,6 +45,7 @@ def print_3_mistakes():
     print("/ | \\      ")
 
 def print_4_mistakes():
+    print("-------------------------------------------")
     print("  |------|-")
     print("  |      | ")
     print("  |      O ")
@@ -51,6 +56,7 @@ def print_4_mistakes():
     print("/ | \\      ")
 
 def print_5_mistakes():
+    print("-------------------------------------------")
     print("  |------|-")
     print("  |      | ")
     print("  |      O ")
@@ -61,6 +67,7 @@ def print_5_mistakes():
     print("/ | \\      ")
 
 def print_6_mistakes():
+    print("-------------------------------------------")
     print("  |------|-")
     print("  |      | ")
     print("  |      O ")
@@ -86,47 +93,111 @@ def print_game_status():
     elif mistakes == 6:
         print_6_mistakes()
 
+    print("-------------------------------------------")
     print("Word: ", end="")
     for element in guesses:
         print(f"{element} ", end='')
     print(f"\nYou have {remaining_guesses} guess(es) remaining")
+    print("Guesses so far: " + str(lettersGuessed))
+    print("-------------------------------------------")
 
 
-
-
-words = ["swordfish", "chillidog", "logarithm", "android", "giraffe", "beautiful", "rhythm"]
+words = ["swordfish", "chilli-dog", "logarithm", "cheeseburger", 
+"giraffe", "milkshake", "rhythm", "noodle-cup", "stegosaurus", 
+"rubik's cube", "Python", "Data Structure", "Algorithm"]
 guesses = []
+lettersGuessed = []
 remaining_guesses = 6
 mistakes = 0
 word_index = random.randint(0, len(words)-1)
 word = words[word_index].upper()
-print(word)
 
 for i in range(len(word)):
-    guesses.append('_')
+    if word[i].isalpha():
+        guesses.append("_")
+    elif word[i] == " ":
+        guesses.append(" ")
+    elif word[i] == "'":
+        guesses.append("'")
+    else:
+        guesses.append("-")
+
+print("-------------------------------------------")
+print("""Let's Play Hangman! 
+Guess the randomly assigned word within a limited amount of
+guesses and save the poor stickman from his doom!
+
+Rules: 
+- Only Letters count as valid inputs.
+- If a word is used as an input, only the first letter will 
+  be used.
+- Letters are not case sensitive.
+- Letters can only be guessed once. Repeats do not count
+  against you. 
+- After 6 failed guesses, you lose!
+- Guess every letter correctly to win.
+
+  Good luck!
+  """)
+print("-------------------------------------------")
+
+startGame = False
+
+while not startGame:
+    user_input = input("Type 'Y' to Start: ")
+    if user_input.upper() != 'Y':
+        pass
+    else:
+        print("-------------------------------------------")
+        print("Save the stickman from his doom!")
+        startGame = True
+
 
 game_over = False
-
 while not game_over:
     print_game_status()
 
     user_input = input("Please enter a letter:\n")
+    
 
     if not user_input.isalpha():
+        print("-------------------------------------------")
         print("That's not a valid input. Please try again.")
+    elif user_input in lettersGuessed:
+        print("-------------------------------------------")
+        print("You have already guessed that letter!")
     else:
         letter = user_input[0].upper()
+        lettersGuessed.append(user_input[0])
         if letter in word:
             for i in range(len(word)):
                 if word[i] == letter:
                     guesses[i] = letter
+                    print("-------------------------------------------")
+                    print("Good guess! " + guesses[i] + " is a part of the word!")
             if "_" not in guesses:
                 game_over = True
+
         else:
-            print("Sorry, that's not part of the word!")
+            print("-------------------------------------------")
+            print("Sorry, that's not part of of the word!")
             remaining_guesses -= 1
             mistakes += 1
             if mistakes == 6:
                 game_over = True
 
-print_game_status()
+
+
+
+if mistakes == 6:
+    print("--------------------------------------")
+    print("You Lose. Please Try Again!")
+    print("The Correct Answer Was: " + word + "!")
+    print("You Guessed: " + str(" ".join(guesses)))
+    print("--------------------------------------")
+else: 
+    print("--------------------------------------")
+    print("Congratulations! You Won!")
+    print("The Correct Answer Was: " + word + "!") 
+    print("You Guess Like a Pro! The Stickman is Saved!")
+    print("--------------------------------------")
